32f2cb56d6db85789d7f2e4dd404baeff8d261a3
Cristian Buleandra <cristian.buleandra@gmail.com>
2021-03-12 22:07:33 +0100

    Add example .bat files to run train/test easier.

---
+++ test.bat
@@ -0,0 +1,8 @@
+python run.py ^
+--data_root data/images ^
+--mask_root data/masks ^
+--result_save_path ./ ^
+--model_path checkpoint/g_final_600209.pth ^
+--test ^
+--target_size 256 ^
+--mask_mode 2 ^
---
+++ train.bat
@@ -0,0 +1,14 @@
+python run.py ^
+--data_root data_train/images ^
+--mask_root data_train/masks ^
+--model_path checkpoint/g_final_600209.pth ^
+--target_size 256 ^
+--mask_mode 1 ^
+--num_iters 600350 ^
+--batch_size 6 ^
+--batch_preload_count 1 ^
+--gpu 0
+
+rem --finetune ^ 
+rem --finetune ^
+rem --batch_size 6 ^
